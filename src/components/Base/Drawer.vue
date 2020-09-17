217701cdf2202c16aedec2e686fe79c3f5d61244
JMaylor <joseph.maylor@gmail.com>
2020-09-17 15:42:17 +0100

    add logout button

--- src/components/Base/Drawer.vue
+++ src/components/Base/Drawer.vue
@@ -1,29 +1,60 @@
 <template>
 	<v-navigation-drawer v-model="$store.state.drawer" app>
-		<v-list nav dense>
-			<div v-for="(link, i) in links" :key="i">
-				<v-list-item v-if="!link.subLinks" :to="link.to" class="v-list-item">
-					<v-list-item-icon>
-						<v-icon>{{ link.icon }}</v-icon>
-					</v-list-item-icon>
-
-					<v-list-item-title v-text="link.text" />
-				</v-list-item>
-
-				<v-list-group v-else :key="link.text" no-action :prepend-icon="link.icon" :value="true">
-					<template v-slot:activator>
-						<v-list-item-title>{{ link.text }}</v-list-item-title>
-					</template>
-
-					<v-list-item v-for="sublink in link.subLinks" :to="sublink.to" :key="sublink.text">
+		<v-layout align-left justify-space-between column fill-height
+			><v-list nav dense>
+				<div v-for="(link, i) in links" :key="i">
+					<v-list-item
+						v-if="!link.subLinks"
+						:to="link.to"
+						class="v-list-item"
+					>
 						<v-list-item-icon>
-							<v-icon>{{ sublink.icon }}</v-icon>
+							<v-icon color="accent">{{ link.icon }}</v-icon>
 						</v-list-item-icon>
-						<v-list-item-title>{{ sublink.text }}</v-list-item-title>
+
+						<v-list-item-title v-text="link.text" />
 					</v-list-item>
-				</v-list-group>
-			</div>
-		</v-list>
+
+					<v-list-group
+						v-else
+						:key="link.text"
+						no-action
+						:prepend-icon="link.icon"
+						:value="true"
+					>
+						<template v-slot:prepend-icon>
+							<v-icon color="accent">{{ link.icon }}</v-icon>
+						</template>
+						<template v-slot:activator>
+							<v-list-item-title>{{
+								link.text
+							}}</v-list-item-title>
+						</template>
+
+						<v-list-item
+							v-for="sublink in link.subLinks"
+							:to="sublink.to"
+							:key="sublink.text"
+						>
+							<v-list-item-icon>
+								<v-icon color="accent">{{ sublink.icon }}</v-icon>
+							</v-list-item-icon>
+							<v-list-item-title>{{
+								sublink.text
+							}}</v-list-item-title>
+						</v-list-item>
+					</v-list-group>
+				</div>
+			</v-list>
+			<v-list dense>
+				<v-list-item class="v-list-item" @click="$store.dispatch('logout')">
+					<v-list-item-icon>
+						<v-icon color="accent">mdi-logout</v-icon>
+					</v-list-item-icon>
+					<v-list-item-title>Logout</v-list-item-title>
+				</v-list-item>
+			</v-list>
+		</v-layout>
 	</v-navigation-drawer>
 </template>
 
